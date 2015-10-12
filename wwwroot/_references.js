ca9f9368c4db0fc9ae12b83f5ecd252600c1739c
reeseoo <reesejmorris@gmail.com>
2015-10-12 21:29:05 +0100

    Added angular to the frontend to interact with OpenBook API.

--- wwwroot/_references.js
+++ wwwroot/_references.js
@@ -1,3 +1,5 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
+/// <reference path="js/angular.min.js" />
+/// <reference path="js/main.ctrl.js" />
 /// <reference path="js/site.js" />
---
+++ wwwroot/js/main.ctrl.js
@@ -0,0 +1,32 @@
+﻿angular.module('OpenBook').controller("MainController", function ($scope, $http) {
+    var vm = this;
+
+    vm.title = 'OpenBook';
+
+    vm.snippetContent = '';
+    vm.snippetAuthor = '';
+
+    $http.get('http://localhost:49629/api/snippet').success(function (data, status, headers, config) {
+        alert("successsssss");
+        $scope.snippets = data;
+    }).error(function (data, status, headers, config) {
+        alert("Unable to get snippets");
+    });
+
+    $scope.submit = function()
+    {   
+        debugger;
+
+        var snippet = {
+            'SnippetContent': $scope.snippetContent,
+            'SnippetAuthor': $scope.snippetAuthor
+        }
+
+        $http.post('http://localhost:49629/api/snippet', snippet, {
+        }).success(function (data, status, headers, config) {
+            alert("Snippet added successfully");
+        }).error(function (data, status, headers, config) {
+            alert("Error");
+        });
+    }
+});
