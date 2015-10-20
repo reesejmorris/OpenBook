751456067ea59b1ac8555294a8e41c512c6dd72b
reeseoo <reesejmorris@gmail.com>
2015-10-20 20:28:33 +0000

    Updated the index page to have multiple controllers. Updated styling slightly.

--- wwwroot/_references.js
+++ wwwroot/_references.js
@@ -3,3 +3,5 @@
 /// <reference path="js/angular.min.js" />
 /// <reference path="js/main.ctrl.js" />
 /// <reference path="js/site.js" />
+/// <reference path="js/snippet.ctrl.js" />
+/// <reference path="js/story.ctrl.js" />
---
+++ wwwroot/js/story.ctrl.js
@@ -0,0 +1,12 @@
+﻿angular.module('OpenBook').controller("StoryController", function ($scope, $http) {
+    var vm = this;
+
+    $http.get('http://localhost:49629/api/story/537aaabe-1370-455e-9921-90b2ba81cc44').success(function (data, status, headers, config) {
+        alert("Got them stories bruh")
+        $scope.story = data;
+    }).error(function (data, status, headers, config) {
+        alert("Unable to retrieve story");
+    });
+
+    vm.title = $scope.story;
+});
